1c463f9c585932227decb1cd0c6aadfe05973c10
Jacque Keener <jvk190000@utdallas.edu>
2020-10-05 20:15:51 -0500

    Part A Done

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentscore = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+
+    public void ExitLevel()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore");
+
+        if(_currentscore > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", _currentscore);
+            Debug.Log("New high score: " + _currentscore);
+        }
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        _currentscore += scoreIncrease;
+        _currentScoreTextView.text = "Score: " + _currentscore.ToString();
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+
+
+        if(_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
